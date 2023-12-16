e0e94b4512e44cf273640e8907e95780e16d0144
Dvwhite81 <123031200+Dvwhite81@users.noreply.github.com>
2023-12-15 23:58:27 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import Main from './components/Main/Main';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('container')).render(<Main />);
